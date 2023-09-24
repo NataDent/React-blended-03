43f300a463867bdb2d4abb5d616d23d43d674247
Mykola Ts <tsybulskiyk@gmail.com>
2023-09-24 15:56:23 +0300

    task-2

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,27 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
-export const SearchForm = () => {
+export const SearchForm = ({onSelect}) => {
+const [continent, setContinent] = useState('');
+
+const onChange = evt => {
+  setContinent(evt.target.value);
+}
+
+const onSubmit = evt => {
+evt.preventDefault();
+
+  onSelect(continent);
+}
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={onSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select defaultValue="default" onChange={onChange}  aria-label="select" name="region" required>
+        <option disabled value="default">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -2,17 +2,43 @@ import {
   Container,
   SearchForm,
   Section,
-  Heading,
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+const [params, setParams] = useSearchParams()
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+useEffect(()=> {
+  const region = params.get('value')
+
+  if(!region) {
+    return
+  }
+
+  fetchByRegion(region).then(setCountries).catch(error => setError(error.message))
+  .finally(setIsLoading(false));
+},[params])
+
+const onSelect = value => {
+  setParams({value})
+}
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSelect={onSelect}/>
+        <CountryList countries={countries} />
       </Container>
+
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
     </Section>
   );
 };
